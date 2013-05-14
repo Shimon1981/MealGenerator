69ee367d184875063cab08298e82f04a36c45c33
Shimon Shvartsbroit <shimon1981@gmail.com>
2013-05-14 18:39:31 +0300

    project created

---
+++ MealGenerator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MealGenerator
+//
+//  Created by Shimon Shvartsbroit on 5/14/13.
+//  Copyright (c) 2013 Shimon Shvartsbroit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
